20bd4ee39bb2f8eb2c5415994f5f7772e104a710
Cesar Henrique Comin <chcomin@gmail.com>
2024-12-19 18:59:04 -0300

    Trained on the DRIVE data

---
+++ experimenters/vessel/trainer.py
@@ -0,0 +1,8 @@
+from torchtrainer.trainer import DefaultTrainer
+
+class VesselTrainer(DefaultTrainer):
+    pass
+
+if __name__ == '__main__':
+    # This is required to run the script from the command line
+    VesselTrainer().fit()
