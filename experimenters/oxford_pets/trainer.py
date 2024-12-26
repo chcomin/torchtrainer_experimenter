05612d2ffe6394331724f7c50dff3d837ff1924e
Cesar Henrique Comin <chcomin@gmail.com>
2024-12-26 16:11:56 -0300

    Examples updated to reflect torchtrainer changes

--- experimenters/oxford_pets/trainer.py
+++
@@ -1,8 +0,0 @@
-from torchtrainer.trainer import DefaultTrainer
-
-class OxfordTrainer(DefaultTrainer):
-    pass
-
-if __name__ == '__main__':
-    # This is required to run the script from the command line
-    OxfordTrainer().fit()
