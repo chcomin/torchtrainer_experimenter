fe1883c8aa8a18072bce580c8a5ab47c57612817
Cesar Henrique Comin <chcomin@gmail.com>
2024-12-27 21:32:27 -0300

    Change single to double quotes

--- experimenters/drive/train.py
+++ experimenters/drive/train.py
@@ -1,8 +1,9 @@
 from torchtrainer.train import DefaultTrainer
 
+
 class VesselTrainer(DefaultTrainer):
     pass
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # This is required to run the script from the command line
     VesselTrainer().fit()
